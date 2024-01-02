8a2ac1c035898f0a6c86eff33727bcb482777a1f
vardyng <wadee@wadeenghoiwa.com>
2024-01-02 15:19:42 -0500

    Add ec2 resource for openvpn

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+locals {
+  user_data_content = templatefile("user_data.sh.template", {
+    AUTO_INSTALL = "y"
+  })
+}
+
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+  
+  name = "${var.app_name}-openvpn"
+
+  instance_type               = var.instance_type
+  key_name                    = var.ec2_key_name
+  monitoring                  = true
+  vpc_security_group_ids      = [module.vpn-service-security-group.security_group_id]
+  subnet_id                   = module.vpc.public_subnets[0]
+  ami                         = var.ec2_ami_id
+  # user_data                   = "${file("user_data.sh")}"
+  user_data                   = local.user_data_content
+  associate_public_ip_address = true
+}
